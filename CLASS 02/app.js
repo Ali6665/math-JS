f5414ecce0f7f764e6d7597157607f2295dc4477
Muhammad Ali <bdon77150@gmail.com>
2025-02-17 21:00:24 +0900

    Add files via upload
    chp 5 to 9

---
+++ CLASS 02/app.js
@@ -0,0 +1,155 @@
+// ch- no. 05
+
+
+// Q .01
+var abc = 3;
+var xyz = 5;
+var egf = xyz + abc;
+document.write("<h1>Q .01</h1> 1." ,"<b>The Sum of 3 and 5 is =</b>" ," ", " ",egf ,);
+
+
+// Q . 02
+
+// Step 01
+
+var abc = 3;
+var xyz = 5;
+var egf = xyz - abc;
+document.write("<br> <br> <br>","<h1>Q .02</h1> 1." ,"<b>The subtraction of 3 and 5 is =</b>" ," ", " ",egf ,);
+
+// Step 02
+
+var abc = 3;
+var xyz = 5;
+var egf = xyz * abc;
+document.write("<br>","2." ,"<b>The multiplication of 3 and 5 is =</b>" ," ", " ",egf ,);
+
+// Step 03
+
+var abc = 3;
+var xyz = 5;
+var egf = xyz % abc;
+document.write("<br>","2." ,"<b>The division of 3 and 5 is =</b>" ," ", " ",egf ,);
+
+
+// Q .03
+
+var ali;
+document.write("<h1>Q3</h1><br>",",<b>Variable declaration is</b> = " , ali );
+
+// Step 02
+
+var ali2 = 10;
+document.write("<br>","<b>Initial value is</b> = " , ali2);
+
+// Step 03
+var ali3 = 5; 
+document.write("<br>","<b>  value is</b> = " , ali3);
+
+var ali4 = 5; ++ali4;
+document.write("<br>","<b> Increment value is</b> = " , ali4);
+
+var a = 5;
+
+var c = ++a + 7;
+document.write("<br>","<b> Add value is</b> = ",c)
+
+// Q . 04
+var x = 600;
+var y = 5;
+var z = x * y;
+document.write("<br><h1>Q 04</h1>","<b>Total cost Buy 5 tickets to a movies is =</b>" ," ",z);
+
+// Q 05
+
+document.write("<br><h1>Q 05</h1>", "Table of 4");
+
+var four1 = 4*1;
+document.write("<br>","<b>4 x 1 =</b>"," ",four1);
+
+var four2 = 4*2;
+document.write("<br>","<b>4 x 2 =</b>"," ",four2);
+
+var four3 = 4*3;
+document.write("<br>","<b>4 x 3 =</b>"," ",four3);
+
+var four4 = 4*4;
+document.write("<br>","<b>4 x 4 =</b>"," ",four4);
+
+var four5 = 4*5;
+document.write("<br>","<b>4 x 5 =</b>"," ",four5);
+
+var four6 = 4*6;
+document.write("<br>","<b>4 x 6 =</b>"," ",four6);
+
+var four7 = 4*7;
+document.write("<br>","<b>4 x 7 =</b>"," ",four7);
+
+var four8 = 4*8;
+document.write("<br>","<b>4 x 8 =</b>"," ",four8);
+
+var four9 = 4*9;
+document.write("<br>","<b>4 x 9 =</b>"," ",four9);
+
+var four10 = 4*10;
+document.write("<br>","<b>4 x 10 =</b>"," ",four10);
+
+// Q 06
+// no answer
+
+
+// Q 07
+
+document.write("<h1>Q 07</h1>");
+document.write("<br><h1> Shopping Center</h1>");
+
+var item1 = 650;
+document.write("<br>","<i>Price of item 1 is</i> = ",item1);
+var item2 = 3;
+document.write("<br>","<i>Quantity of item 1 is</i> = ",item2);
+var item3 = 100;
+document.write("<br>","<i>Price of item 2 is</i> = ",item3);
+var item4 = 7;
+document.write("<br>","<i>Price of item 2 is</i> = ",item4);
+var item5 = 2750;
+document.write("<br><br>","<b>Total cost of your order</b> = ",item5);
+
+
+// Q 08
+
+document.write("<h1>Q 08</h1>");
+document.write("<br><h1>Mark Sheet</h1>");
+
+var mark1 = 980;
+document.write("<br>","<i>Price of item 1 is</i> = ",mark1);
+var mark2 = 804;
+document.write("<br>","<i>Quantity of item 1 is</i> = ",mark2);
+var permarks = (mark2/mark1)*100;
+document.write("<br><i>Percentage</i>:" , " " , permarks);
+
+// Q 09
+// no answer
+
+
+
+// Q 10 
+
+
+document.write("<br> <h1>Q 10</h1>");
+var ahmed = 17;
+var ahmedadd = ahmed + 5 * 10 / 2;
+document.write("Answer is ="," ",ahmed);
+
+// Q 11
+
+document.write("<br> <h1>Q 11</h1>")
+
+
+var date_brith = 1992;
+var perant_year = 2016;
+var Age = perant_year - date_brith;
+document.write("Your age ",Age)
+
+
+
+
