7eec5e5c344202a7d501ad7f0b8b447e64a17e97
Muhammad Ali <bdon77150@gmail.com>
2025-02-20 07:00:40 +0900

    Update app.js

--- CLASS 02/app.js
+++ CLASS 02/app.js
@@ -156,10 +156,87 @@ document.write("Your age ",Age)
 
 
 
-//ch-no. 06
+// Ch- 6-9 pdf
 
-document.write("<center><h1>ch 6 to 9 "pdf"</h1></center>");
+//Q 01
+document.write("<h1>Q 01</h1>a is");
 
+var alii12 = 2, alii13 = 1;
+document.write(" ",alii12);
+
+var alii14 = --alii12 - --alii13 + ++alii13 + alii13--;
+document.write("<br><br>b is"," ",alii13);
+document.write("<br><br>Result is"," ",alii14);
+
+
+//Q 02
+document.write("<h1>Q10</h1>");
+
+var alii15 = prompt("Enter your name:");
+document.write("<h2>, " + alii15);
+
+
+// Q 05
+
+var aliinum = +prompt("Enter a number to generate multiplication table");
+
+document.write(
+  aliinum," x 1 = ",
+  aliinum * 1,"<br>",
+  aliinum,"x 2 = ",
+  aliinum * 2, "<br>",
+  aliinum,"x 3 = ",
+  aliinum * 3, "<br>",
+  aliinum,"x 4 = ",
+  aliinum * 4,"<br>",
+  aliinum, "x 5 = ",
+  aliinum * 5,"<br>",
+  aliinum,"x 6 = ",
+  aliinum * 6, "<br>",
+  aliinum,"x 7 = ",
+  aliinum * 7, "<br>",
+  aliinum, "x 8 = ",
+  aliinum * 8,"<br>",
+  aliinum,"x 9 = ", aliinum * 9
+);
+
+
+
+
+
+// Q 06
+document.write("<br><h1>Q  12</h1>");
+
+
+var sub1 = prompt("Enter your 1st subject name:");
+var sub2 = prompt("Enter your 2nd subject name:");
+var sub3 = prompt("Enter your 3rd subject name:");
+
+var sub_marks1 = +prompt("Enter your first_sub_marks " + sub1);
+var sub_marks2 = +prompt("Enter your second_sub_marks " + sub2);
+var sub_marks3 = +prompt("Enter your third_sub_marks  " + subt3);
+
+var totalMarks = 100;
+var totalObtained = sub_marks1  + sub_marks1  + sub_marks1 
+var percentage = (totalObtained/(totalMarks * 3)*100)
+
+document.write("<table>","<tr><th>Subject</th><th>Total Marks</th><b><th>Obtained Marks</th><b><th>Percentage</th></b></tr>","<tr><td>",sub1,"</td><td>",totalMarks,"</td><td>",sub_marks1,"</td><td>",((sub_marks1 / totalMarks) * 100),"%</td></tr>","<tr><td>",sub2,"</td><td>",totalMarks,"</td><td>",sub_marks2,"</td><td>",((sub_marks2 / totalMarks) * 100),"%</td></tr>","<tr><td>",sub3,"</td><td>",totalMarks,"</td><td>",sub_marks3,"</td><td>",((sub_marks3 / totalMarks) * 100),"%</td></tr>","<tr><th>Total</th><th>",(totalMarks*3),"</th><th>",totalObtained,"</th><th>",percentage,"</th></tr>","</table>");
+
+
+
+
+
+
+
+
+
+
+
+//ch-no. 06-06 Home pdf
+
+document.write("<center><h1>ch 6 to 9 "Home pdf"</h1></center>");
+
+// Ch 06
 //Q 01
 document.write("<br><h1>Q1</h1>");
 
@@ -175,7 +252,7 @@ document.write(alii);
 //Q 03
 document.write("<br><h1>Q3</h1>");
 
-var alii2 = 100;
+var alii2 = 50;
 var alii3 = alii2++;
 document.write(alii3);
 
@@ -183,7 +260,7 @@ document.write(alii3);
 //Q 04
 
 document.write("<br><h1>Q4</h1>")
-var alii5 = 100;
+var alii5 = 50;
 var alii6 = --alii5;
 document.write(alii6);
 document.write(alii6);
@@ -193,38 +270,104 @@ document.write(alii6);
 
 //Q 05
 document.write("<br><h1>Q5</h1>");
-var alii7 = 2;
+var alii7 = 10;
 var alii8 = alii7--; 
 document.write( alii8);
 
 
 
 //Q 06
-var alii9 = 10;
-var alii10 = alii9++, ali_10 = alii9 - 1;
-document.write("<br><h1>Q6</h1>","Answer in app.js");
+
+document.write("<br><h1>Q6</h1>");
+var alii9 = 15;
+var alii10 = alii9++, ali10 = alii9 * 1;
+
 
 
 
 //Q 07
-var alii11 = 10;  
+document.write("<br><h1>Q7</h1>",);
+
+var alii11 = 15;  
 alii11++;          
 alert(alii11); 
-document.write("<br><h1>Q7</h1>","Answer in app.js");
+
+// end Ch 06
+
+
+
+// Ch 07 
+document.write("Chapter 7 Math Expression ")
+
+
+
+//Q 01
+document.write("<h1>Q 01</h1>",aliNum);
+
+var aliNum = 2 + (2 * 6);
+document.write(aliNum);
+
+
+
+//Q 03
+document.write("<br><h1>Q 03</h1>");
+
+var aliNum2 = (2 + 2) * (4 + 2);
+document.write("<h1>Q12</h1>",aliNum2);
+
+//Q 04
+
+document.write("<br><h1>Q 04</h1>");
+
+var aliNum3 = ((2 + 2) * 4) + 2;
+document.write(aliNum3);
+
+
+//Q 05
+document.write("<br><h1>Q 05</h1>");
+
+var ali_C = (2 + 2) * (4 + 10);
+document.write(ali_C);
+
+//Q 06
+document.write("<br><h1>Q15</h1>");
+
+var ali_U = 2 + 2  *  4 + 10;
+document.write( ali_U);
+
+//Q 07
+document.write("<br><h1>Q 07</h1>");
+var ali_P = 4 / 2 * 4;
+document.write(ali_P);
 
 
 
 
+// Ch 08 Concatenating Text Strings
+
+//Q 08
+document.write("<h1>Q17</h1>");
+
+var zxc = "2" + "2";
+document.write(zxc);
 
 //Q 09
-document.write("<h1>Q9</h1>a is");
+document.write("<h1>Q18</h1>");
 
-var alii12 = 2, alii13 = 1;
-document.write(" ",alii12);
+var mnb =("Hello","Dolly");
+document.write(mnb);
 
-var alii14 = --alii12 - --alii13 + ++alii13 + alii13--;
-document.write("<br><br>b is"," ",alii13);
-document.write("<br><br>Result is"," ",alii14);
+
+// Q 10
+document.write("<h1>Q18</h1>");
+
+var mnb =("Pakistan Zindabad");
+document.write(mnb);
+
+
+
+// end
+
 
 
 
